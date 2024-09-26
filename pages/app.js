0c4cd8d91d6d9527d77d86d1635e6e8ebd49ba3f
aymanzakaria641 <aymanzakaria641@gmail.com>
2024-09-26 22:52:59 +0300

    first commit

---
+++ pages/app.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("scroll", ()=> {
+    if (window.scrollY > 100) {
+        header.classList.add("scroll-down");
+    }
+    else {
+        header.classList.remove("scroll-down");
+    }
+})
